f6f0cc1431323e7e5e628e941cb88fc2469e1778
Miran <Miran4@op.pl>
2017-03-24 02:55:11 +0100

    Testing geolocation.

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,30 +1,25 @@
-function init() {
+function init() 
+{
 	document.addEventListener("deviceready",onDeviceReady, false);
 }
 
-function onDeviceReady() {
-	navigator.notification.beep(2);
-	deviceInfo();
-}
-
-function deviceInfo() {
-
-	info =  'Device Model   : '    + device.model + '<br>' + 
-			'Device Name    : '     + device.name + '<br>' + 
-			'Device Cordova : '  + device.cordova + '<br>' + 
-			'Device Platform: ' + device.platform + '<br>' + 
-			'Device UUID    : '     + device.uuid + '<br>' + 
-			'Device Version : '  + device.version + '<br>';
-
-	document.getElementById("deviceDetails").innerHTML = info;	
+function onDeviceReady() 
+{
+	//navigator.notification.beep(2);
 }
 
 function addNewLocation()
 {
 	var imgData = ''
-	
 	navigator.camera.getPicture(
 		function(val) { imgData = val; alert('Photo OK'); },
-		function(val) { alert('Failed because: ' + val); }
+		function(val) { alert('Failed to get photo: ' + val); }
+	);
+	
+	var lon = 0;
+	var lat = 0;
+	navigator.geolocation.getCurrentPosition(
+		function(val) { lon = val.coords.latitude; val.coords.longitude; alert('Position:' + lon + ", " + lat); },
+		function(val) { alert('Failed to get geolocation: ' + val); }
 	);
 }
