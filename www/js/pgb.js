86bb5905723ca503c04023a73b4cd6a1d6e9b7dd
Miran <Miran4@op.pl>
2017-04-24 22:37:10 +0200

    Updates in UI.

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,11 +1,26 @@
 function init() 
 {
 	document.addEventListener("deviceready",onDeviceReady, false);
+	updateCompass();
 }
 
 function onDeviceReady() 
 {
 	//navigator.notification.beep(2);
+}
+
+function updateCompass()
+{
+	var height = window.innerHeight;
+	height -= document.getElementById("navHeader").offsetHeight;
+	height -= document.getElementById("navInfo").offsetHeight;
+	height -= document.getElementById("navFooter").offsetHeight;
+	height += 50;
+	
+	document.getElementById("navCompass").height = height + "px";
+	
+	var needle = document.getElementById("needle");
+	needle.setAttribute("transform", "rotate(0 60 60) translate(100, 100) ");
 }
 
 function addNewLocation()
