3488c045aeb53683a60e34dd63b29d9ab5dfcee3
Miran <Miran4@op.pl>
2017-04-27 02:40:51 +0200

    debuging file listing

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -142,12 +142,12 @@ function updateLocationsList()
 								// filename format : lon_lat_description.jpg
 								var filename = entries[i].name;
 								
-								alert(filename.slice(-4));
-								
-								if(filename.slice(-4) != ".jpg")
+								if(!filename.endswith(".jpg"))
 								{
 									continue; // not jpg file
 								}
+								
+								alert("k");
 																
 								filename = filename.substr(0, filename.length-4); // remove extension
 								var filenameParts = mainStr.split("_");
