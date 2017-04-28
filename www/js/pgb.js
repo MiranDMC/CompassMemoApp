8921c99089206baade69c80d2ecb6a0e9ab2d0ed
Miran <Miran4@op.pl>
2017-04-28 07:29:04 +0200

    Implementing distance calculations.

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -52,16 +52,28 @@ function geoAngleFromCoordinate(lon1, lat1, lon2, lat2)
 	lon2 = lon2.toRad();
 	lat2 = lat2.toRad();
 	
+	alert(lon1 + ' ' + lat1 + ' ' + lon2 + ' ' + lat2);
+	
     var dLon = (lon2 - lon1);
+	alert(dLon);
 
     var y = Math.sin(dLon) * Math.cos(lat2);
-    var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
-            * Math.cos(lat2) * Math.cos(dLon);
+    var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+	
+	alert(y,x);
 
     var brng = Math.atan2(y, x);
+	
+	alert(brng);
 
     brng = brng.toDeeg();
+	
+	alert(brng);
+	
     brng = (brng + 360.0) % 360.0;
+	
+	alert(brng);
+	
     brng = 360.0 - brng; // count degrees counter-clockwise - remove to make clockwise
 
     return brng;
